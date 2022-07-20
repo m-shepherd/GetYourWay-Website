aacf540b9c4df17587ecabaf107efb1608b2b0a3
m-shepherd <michael.shepherd2@sky.uk>
2022-07-20 15:53:47 +0100

    Moved code for login into its own component

--- src/main/front-end/src/App/App.js
+++ src/main/front-end/src/App/App.js
@@ -1,36 +1,11 @@
 import './App.css';
 
+import Login from '../Login/Login';
+
 const App = () => {
   return (
     <div className="container">
-      <h1>Login Page</h1>
-      <form>
-        <label for="fname">
-          First Name:<br></br>
-          <input type="text" id="fname" name="fname" />
-          <br></br>
-        </label>
-        <label for="lname">
-          Last Name:<br></br>
-          <input type="text" id="lname" name="lname" />
-          <br></br>
-        </label>
-        <label for="email">
-          Email:<br></br>
-          <input type="text" id="email" name="email" />
-          <br></br>
-        </label>
-        <label for="uname">
-          Username:<br></br>
-          <input type="text" id="uname" name="uname" />
-          <br></br>
-        </label>
-        <label for="pword">
-          Password:<br></br>
-          <input type="text" id="pword" name="pword" />
-          <br></br>
-        </label>
-      </form>
+      <Login />
     </div>
   );
 }
