d5faafba39a9c72281c4b8595267823e6010770d
m-shepherd <michael.shepherd2@sky.uk>
2022-07-20 15:25:42 +0100

    Converted javascript project into React project

---
+++ src/main/front-end/src/App/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Login Page</h1>
+      <form>
+        <label for="fname">
+          First Name:<br></br>
+          <input type="text" id="fname" name="fname" />
+          <br></br>
+        </label>
+        <label for="lname">
+          Last Name:<br></br>
+          <input type="text" id="lname" name="lname" />
+          <br></br>
+        </label>
+        <label for="email">
+          Email:<br></br>
+          <input type="text" id="email" name="email" />
+          <br></br>
+        </label>
+        <label for="uname">
+          Username:<br></br>
+          <input type="text" id="uname" name="uname" />
+          <br></br>
+        </label>
+        <label for="pword">
+          Password:<br></br>
+          <input type="text" id="pword" name="pword" />
+          <br></br>
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default App;
