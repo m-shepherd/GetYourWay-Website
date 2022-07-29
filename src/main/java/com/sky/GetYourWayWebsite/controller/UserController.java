2ce06eaa1dc2781e3f31609d01332319640347f3
m-shepherd <michael.shepherd2@sky.uk>
2022-07-29 17:42:56 +0100

    Fixed authorization issue with tests and added some new tests for userController

--- src/main/java/com/sky/GetYourWayWebsite/controller/UserController.java
+++ src/main/java/com/sky/GetYourWayWebsite/controller/UserController.java
@@ -56,7 +56,7 @@ public class UserController {
         if (userService.findByEmail(email).isPresent()){
             return HttpStatus.OK;
         } else {
-            return HttpStatus.BAD_REQUEST;
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -65,7 +65,7 @@ public class UserController {
         if (isUserPresent(newUser.getUsername())) {
             return editUserDetails(newUser);
         } else {
-            return HttpStatus.BAD_REQUEST;
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -74,7 +74,7 @@ public class UserController {
         if (!isUserPresent(newUser.getUsername())) {
             return editUserDetails(newUser);
         } else {
-            return HttpStatus.BAD_REQUEST;
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -86,7 +86,7 @@ public class UserController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid User Information Provided");
         }
         if (result == null) {
-            return HttpStatus.BAD_REQUEST;
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         } else {
             return HttpStatus.CREATED;
         }
